4f4abda357014c5eb9ebc0bdb53a64a954881cdf
Ellen Cornelius <ecorneli@iwu.edu>
2018-04-30 08:10:06 -0600

    Add comment box and form inputs to html.

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').click(function() {
+    var body = $('textarea[name="comment"]').val()
+    var author = $('input[name="author"]')
+    $('.comment_list').append(body + "<span class=name>" + author + "</span")
+  })
 })
